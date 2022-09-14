1e67a235a43aabec467ae617c93e864b7a7e18ef
yuchenyee <yuchen3188@gmail.com>
2022-09-14 23:22:46 +0800

    Update 2022-09-14T15:22:41.573Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,8 +1,11 @@
+"use strict";
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 10,
   spaceBetween: 16,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
-  },
+    clickable: true
+  }
 });
+//# sourceMappingURL=all.js.map
